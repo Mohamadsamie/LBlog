f393822f635ae90d1c6695d28ef3a9f0f1e3c2f5
Mohamad Samie <mohamad.samieshad@gmail.com>
2019-06-20 19:48:33 +0430

    Verta package added and users are showing on admin index.

--- app/Http/Controllers/Admin/AdminUserController.php
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -14,7 +15,8 @@ class AdminUserController extends Controller
      */
     public function index()
     {
-        return  view('admin.users.index');
+        $users = User::all();
+        return  view('admin.users.index', compact(['users']));
     }
 
     /**
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,25 @@
 @extends('admin.layouts.master')
+
+@section('content')
+
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th>نام</th>
+            <th>ایمیل</th>
+            <th>تاریخ عضویت</th>
+        </tr>
+        </thead>
+        <tbody>
+            @foreach($users as $user)
+                <tr>
+                    <td>{{$user->name}}</td>
+                    <td>{{$user->email}}</td>
+                    <td>{{Hekmatinasser\Verta\Verta::instance($user->created_at)->formatDifference(\Hekmatinasser\Verta\Verta::today('Asia/Tehran'))}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+
+@endsection
