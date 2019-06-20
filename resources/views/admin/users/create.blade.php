987bb63683a4053915f5893b2340921882438b5d
Mohamad Samie <mohamad.samieshad@gmail.com>
2019-06-20 16:36:08 +0430

    Admin views, Templates and node modules are added

---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,1 @@
+<h3>admin user create</h3>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,20 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.styles([
+    'resources/admin/css/font-awesome.min.css',
+    'resources/admin/css/simple-line-icons.css',
+    'resources/admin/dist/style.css',
+    'resources/admin/dist/style-custom.css',
+], 'public/css/all.css');
+
+mix.scripts([
+        'resources/admin/js/libs/jquery.min.js',
+        'resources/admin/js/libs/tether.min.js',
+        'resources/admin/js/libs/bootstrap.min.js',
+        'resources/admin/js/libs/pace.min.js',
+        'resources/admin/js/libs/Chart.min.js',
+        'resources/admin/js/app.js',
+        'resources/admin/js/views/main.js',
+    ], 'public/js/all.js');
+
