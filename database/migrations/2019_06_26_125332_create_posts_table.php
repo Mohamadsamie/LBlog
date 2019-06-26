cd3d266b0cfa216994791a4ff0e39b5df20171cc
Mohamad Samie <mohamad.samieshad@gmail.com>
2019-06-26 13:20:29 +0430

    Post,Category models with migrations and relationships are added

--- database/migrations/2019_06_26_125332_create_posts_table.php
+++ database/migrations/2019_06_26_125332_create_posts_table.php
@@ -16,7 +16,16 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
+            $table->string('slug')->unique();
             $table->text('description');
+            $table->text('meta_description');
+            $table->text('meta_keywords');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('photo_id');
+            $table->unsignedInteger('category_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('photo_id')->references('id')->on('photos');
+            $table->foreign('category_id')->references('id')->on('categories');
             $table->timestamps();
         });
     }
